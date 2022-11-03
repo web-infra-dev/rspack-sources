98f5dbff28690f20c1ffe76b44219cdab9060a13
h-a-n-a <andywangsy@gmail.com>
2022-11-03 13:33:18 +0800

    feat: expose `StreamChunks` related types

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,11 @@ mod vlq;
 pub use cached_source::CachedSource;
 pub use concat_source::ConcatSource;
 pub use error::{Error, Result};
+pub use helpers::{
+  decode_mappings, encode_mappings, get_map, stream_chunks_of_raw_source,
+  stream_chunks_of_source_map, GeneratedInfo, OnChunk, OnName, OnSource,
+  StreamChunks,
+};
 pub use original_source::OriginalSource;
 pub use raw_source::RawSource;
 pub use replace_source::ReplaceSource;
