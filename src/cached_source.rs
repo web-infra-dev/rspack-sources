dc6e512bc108055f756df2e98b3a33938179cf87
Hana <andywangsy@gmail.com>
2024-12-16 13:56:59 +0800

    perf: init

--- src/cached_source.rs
+++ src/cached_source.rs
@@ -49,8 +49,16 @@ use crate::{
 ///   "Hello World\nconsole.log('test');\nconsole.log('test2');\nHello2\n"
 /// );
 /// ```
-pub struct CachedSource<T> {
+pub struct CachedSource<T: 'static> {
+  inner: Arc<CachedSourceInner<T>>,
+}
+
+#[ouroboros::self_referencing]
+pub struct CachedSourceInner<T: 'static> {
   inner: Arc<T>,
+  #[not_covariant]
+  #[borrows(inner)]
+  cached_rope: Arc<OnceLock<Rope<'this>>>,
   cached_hash: Arc<OnceLock<u64>>,
   cached_maps:
     Arc<DashMap<MapOptions, Option<SourceMap>, BuildHasherDefault<FxHasher>>>,
@@ -60,29 +68,40 @@ impl<T> CachedSource<T> {
   /// Create a [CachedSource] with the original [Source].
   pub fn new(inner: T) -> Self {
     Self {
-      inner: Arc::new(inner),
-      cached_hash: Default::default(),
-      cached_maps: Default::default(),
+      inner: Arc::new(CachedSourceInner::new(
+        Arc::new(inner),
+        |_| Default::default(),
+        Default::default(),
+        Default::default(),
+      )),
     }
   }
 
   /// Get the original [Source].
   pub fn original(&self) -> &T {
-    &self.inner
+    self.inner.borrow_inner()
+  }
+}
+
+impl<T: Source> CachedSource<T> {
+  fn get_rope(&self) -> &Rope<'_> {
+    self
+      .inner
+      .with(|cache| cache.cached_rope.get_or_init(|| cache.inner.rope()))
   }
 }
 
 impl<T: Source + Hash + PartialEq + Eq + 'static> Source for CachedSource<T> {
   fn source(&self) -> Cow<str> {
-    self.inner.source()
+    Cow::Owned(self.get_rope().to_string())
   }
 
   fn rope(&self) -> Rope<'_> {
-    self.inner.rope()
+    self.get_rope().clone()
   }
 
   fn buffer(&self) -> Cow<[u8]> {
-    self.inner.buffer()
+    self.inner.borrow_inner().buffer()
   }
 
   fn size(&self) -> usize {
@@ -90,17 +109,20 @@ impl<T: Source + Hash + PartialEq + Eq + 'static> Source for CachedSource<T> {
   }
 
   fn map(&self, options: &MapOptions) -> Option<SourceMap> {
-    if let Some(map) = self.cached_maps.get(options) {
+    if let Some(map) = self.inner.borrow_cached_maps().get(options) {
       map.clone()
     } else {
-      let map = self.inner.map(options);
-      self.cached_maps.insert(options.clone(), map.clone());
+      let map = self.inner.borrow_inner().map(options);
+      self
+        .inner
+        .borrow_cached_maps()
+        .insert(options.clone(), map.clone());
       map
     }
   }
 
   fn to_writer(&self, writer: &mut dyn std::io::Write) -> std::io::Result<()> {
-    self.inner.to_writer(writer)
+    self.inner.borrow_inner().to_writer(writer)
   }
 }
 
@@ -114,10 +136,10 @@ impl<T: Source + Hash + PartialEq + Eq + 'static> StreamChunks
     on_source: crate::helpers::OnSource<'_, 'a>,
     on_name: crate::helpers::OnName<'_, 'a>,
   ) -> crate::helpers::GeneratedInfo {
-    let cached_map = self.cached_maps.entry(options.clone());
+    let cached_map = self.inner.borrow_cached_maps().entry(options.clone());
     match cached_map {
       Entry::Occupied(entry) => {
-        let source = self.rope();
+        let source = self.get_rope();
         if let Some(map) = entry.get() {
           #[allow(unsafe_code)]
           // SAFETY: We guarantee that once a `SourceMap` is stored in the cache, it will never be removed.
@@ -128,17 +150,26 @@ impl<T: Source + Hash + PartialEq + Eq + 'static> StreamChunks
           let map =
             unsafe { std::mem::transmute::<&SourceMap, &'a SourceMap>(map) };
           stream_chunks_of_source_map(
-            source, map, on_chunk, on_source, on_name, options,
+            source.clone(),
+            map,
+            on_chunk,
+            on_source,
+            on_name,
+            options,
           )
         } else {
           stream_chunks_of_raw_source(
-            source, options, on_chunk, on_source, on_name,
+            source.clone(),
+            options,
+            on_chunk,
+            on_source,
+            on_name,
           )
         }
       }
       Entry::Vacant(entry) => {
         let (generated_info, map) = stream_and_get_source_and_map(
-          &self.inner as &T,
+          self.inner.borrow_inner() as &T,
           options,
           on_chunk,
           on_source,
@@ -155,17 +186,15 @@ impl<T> Clone for CachedSource<T> {
   fn clone(&self) -> Self {
     Self {
       inner: self.inner.clone(),
-      cached_hash: self.cached_hash.clone(),
-      cached_maps: self.cached_maps.clone(),
     }
   }
 }
 
 impl<T: Source + Hash + PartialEq + Eq + 'static> Hash for CachedSource<T> {
   fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-    (self.cached_hash.get_or_init(|| {
+    (self.inner.borrow_cached_hash().get_or_init(|| {
       let mut hasher = FxHasher::default();
-      self.inner.hash(&mut hasher);
+      self.original().hash(&mut hasher);
       hasher.finish()
     }))
     .hash(state);
@@ -174,7 +203,7 @@ impl<T: Source + Hash + PartialEq + Eq + 'static> Hash for CachedSource<T> {
 
 impl<T: PartialEq> PartialEq for CachedSource<T> {
   fn eq(&self, other: &Self) -> bool {
-    self.inner == other.inner
+    self.inner.borrow_inner() == other.inner.borrow_inner()
   }
 }
 
@@ -186,9 +215,12 @@ impl<T: std::fmt::Debug> std::fmt::Debug for CachedSource<T> {
     f: &mut std::fmt::Formatter<'_>,
   ) -> Result<(), std::fmt::Error> {
     f.debug_struct("CachedSource")
-      .field("inner", self.inner.as_ref())
-      .field("cached_hash", self.cached_hash.as_ref())
-      .field("cached_maps", &(!self.cached_maps.is_empty()))
+      .field("inner", self.inner.borrow_inner().as_ref())
+      .field("cached_hash", self.inner.borrow_cached_hash().as_ref())
+      .field(
+        "cached_maps",
+        &(!self.inner.borrow_cached_maps().is_empty()),
+      )
       .finish()
   }
 }
@@ -236,7 +268,12 @@ mod tests {
     source.map(&map_options);
 
     assert_eq!(
-      *clone.cached_maps.get(&map_options).unwrap().value(),
+      *clone
+        .inner
+        .borrow_cached_maps()
+        .get(&map_options)
+        .unwrap()
+        .value(),
       source.map(&map_options)
     );
   }
--- src/rope.rs
+++ src/rope.rs
@@ -6,7 +6,7 @@ use std::{
   collections::VecDeque,
   hash::Hash,
   ops::{Bound, RangeBounds},
-  rc::Rc,
+  sync::Arc,
 };
 
 use crate::Error;
@@ -14,7 +14,7 @@ use crate::Error;
 #[derive(Clone, Debug)]
 pub(crate) enum Repr<'a> {
   Light(&'a str),
-  Full(Rc<Vec<(&'a str, usize)>>),
+  Full(Arc<Vec<(&'a str, usize)>>),
 }
 
 /// A rope data structure.
@@ -43,13 +43,13 @@ impl<'a> Rope<'a> {
     match &mut self.repr {
       Repr::Light(s) => {
         let vec = Vec::from_iter([(*s, 0), (value, s.len())]);
-        self.repr = Repr::Full(Rc::new(vec));
+        self.repr = Repr::Full(Arc::new(vec));
       }
       Repr::Full(data) => {
         let len = data
           .last()
           .map_or(0, |(chunk, start_pos)| *start_pos + chunk.len());
-        Rc::make_mut(data).push((value, len));
+        Arc::make_mut(data).push((value, len));
       }
     }
   }
@@ -61,7 +61,7 @@ impl<'a> Rope<'a> {
     match (&mut self.repr, value.repr) {
       (Repr::Light(s), Repr::Light(other)) => {
         let raw = Vec::from_iter([(*s, 0), (other, s.len())]);
-        self.repr = Repr::Full(Rc::new(raw));
+        self.repr = Repr::Full(Arc::new(raw));
       }
       (Repr::Full(s), Repr::Full(other)) => {
         if !other.is_empty() {
@@ -69,7 +69,7 @@ impl<'a> Rope<'a> {
             .last()
             .map_or(0, |(chunk, start_pos)| *start_pos + chunk.len());
 
-          let cur = Rc::make_mut(s);
+          let cur = Arc::make_mut(s);
           cur.reserve_exact(other.len());
 
           for &(chunk, _) in other.iter() {
@@ -83,7 +83,7 @@ impl<'a> Rope<'a> {
           let len = s
             .last()
             .map_or(0, |(chunk, start_pos)| *start_pos + chunk.len());
-          Rc::make_mut(s).push((other, len));
+          Arc::make_mut(s).push((other, len));
         }
       }
       (Repr::Light(s), Repr::Full(other)) => {
@@ -94,7 +94,7 @@ impl<'a> Rope<'a> {
           raw.push((chunk, len));
           len += chunk.len();
         }
-        self.repr = Repr::Full(Rc::new(raw));
+        self.repr = Repr::Full(Arc::new(raw));
       }
     }
   }
@@ -325,7 +325,7 @@ impl<'a> Rope<'a> {
         })?;
 
         Ok(Rope {
-          repr: Repr::Full(Rc::new(raw)),
+          repr: Repr::Full(Arc::new(raw)),
         })
       }
     }
@@ -413,7 +413,7 @@ impl<'a> Rope<'a> {
         });
 
         Rope {
-          repr: Repr::Full(Rc::new(raw)),
+          repr: Repr::Full(Arc::new(raw)),
         }
       }
     }
@@ -614,7 +614,7 @@ impl<'a> Iterator for Lines<'_, 'a> {
           // Advance the byte index to the end of the line.
           *byte_idx += len;
           Some(Rope {
-            repr: Repr::Full(Rc::new(raw)),
+            repr: Repr::Full(Arc::new(raw)),
           })
         } else {
           // If we did not find a newline in the next few chunks,
@@ -646,7 +646,7 @@ impl<'a> Iterator for Lines<'_, 'a> {
           // Advance the byte index to the end of the rope.
           *byte_idx += len;
           Some(Rope {
-            repr: Repr::Full(Rc::new(raw)),
+            repr: Repr::Full(Arc::new(raw)),
           })
         }
       }
@@ -864,7 +864,7 @@ impl<'a> FromIterator<&'a str> for Rope<'a> {
       .collect::<Vec<_>>();
 
     Self {
-      repr: Repr::Full(Rc::new(raw)),
+      repr: Repr::Full(Arc::new(raw)),
     }
   }
 }
@@ -889,7 +889,7 @@ fn end_bound_to_range_end(end: Bound<&usize>) -> Option<usize> {
 
 #[cfg(test)]
 mod tests {
-  use std::rc::Rc;
+  use std::sync::Arc;
 
   use crate::rope::{Repr, Rope};
 
@@ -915,7 +915,7 @@ mod tests {
     assert_eq!(simple, "abcdef");
     assert_eq!(
       simple.repr,
-      Repr::Full(Rc::new(Vec::from_iter([("abc", 0), ("def", 3)])))
+      Repr::Full(Arc::new(Vec::from_iter([("abc", 0), ("def", 3)])))
     );
     assert_eq!(simple.len(), 6);
 
@@ -923,7 +923,7 @@ mod tests {
     assert_eq!(simple, "abcdefghi");
     assert_eq!(
       simple.repr,
-      Repr::Full(Rc::new(Vec::from_iter([
+      Repr::Full(Arc::new(Vec::from_iter([
         ("abc", 0),
         ("def", 3),
         ("ghi", 6),
@@ -946,7 +946,7 @@ mod tests {
     assert_eq!(append1, "abcdef");
     assert_eq!(
       append1.repr,
-      Repr::Full(Rc::new(Vec::from_iter([("abc", 0), ("def", 3),])))
+      Repr::Full(Arc::new(Vec::from_iter([("abc", 0), ("def", 3),])))
     );
 
     // simple - complex
@@ -955,7 +955,7 @@ mod tests {
     assert_eq!(append2, "abc123");
     assert_eq!(
       append2.repr,
-      Repr::Full(Rc::new(Vec::from_iter([
+      Repr::Full(Arc::new(Vec::from_iter([
         ("abc", 0),
         ("1", 3),
         ("2", 4),
@@ -969,7 +969,7 @@ mod tests {
     assert_eq!(append3, "123abc");
     assert_eq!(
       append3.repr,
-      Repr::Full(Rc::new(Vec::from_iter([
+      Repr::Full(Arc::new(Vec::from_iter([
         ("1", 0),
         ("2", 1),
         ("3", 2),
@@ -983,7 +983,7 @@ mod tests {
     assert_eq!(append4, "123456");
     assert_eq!(
       append4.repr,
-      Repr::Full(Rc::new(Vec::from_iter([
+      Repr::Full(Arc::new(Vec::from_iter([
         ("1", 0),
         ("2", 1),
         ("3", 2),
@@ -1071,7 +1071,7 @@ mod tests {
     assert_eq!(rope, "abcdef");
     assert_eq!(
       rope.repr,
-      Repr::Full(Rc::new(Vec::from_iter([("abc", 0), ("def", 3)])))
+      Repr::Full(Arc::new(Vec::from_iter([("abc", 0), ("def", 3)])))
     );
   }
 
